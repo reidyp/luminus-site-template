e607e0231f71c448aa6cb52c8d68ceafc102d0ea
tao <tao.zhou2009@gmail.com>
2014-11-13 14:44:09 +0800

    description and url for project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject luminus-site/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen template for Luminus micro web framework."
+  :url "https://github.com/bluealert/luminus-site-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
